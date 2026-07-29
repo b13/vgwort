6f74681415f67cfb52845c0bff2519b7d3be7ddf
David Steeb <david.steeb@b13.com>
2026-07-29 16:09:42 +0200

    [RELEASE] 0.1.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'typo3@b13.com',
     'author_company' => 'b13 GmbH',
     'state' => 'stable',
-    'version' => '0.1.0',
+    'version' => '0.1.3',
     'constraints' => [
         'depends' => ['typo3' => '13.4.0-14.99.99'],
         'conflicts' => [],
